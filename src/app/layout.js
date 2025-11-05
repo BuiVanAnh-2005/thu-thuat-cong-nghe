59813fc78014708dc92c4d605055cf002aa30a50
Bui Van Anh <avan75954@gmail.com>
2025-11-05 15:58:12 +0700

    setup google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import "../styles/mobile.css";
 import "../styles/desktop.css";
 import ClientLayout from "./ClientLayout";
+import Script from "next/script";
+
 
 export const metadata = {
   title: "TechTips - Trang công nghệ hàng đầu",
@@ -12,6 +14,22 @@ export default function RootLayout({ children }) {
     <html lang="vi">
        <head>
     <link rel="icon" href="/vercel.svg" type="image/svg+xml" />
+     {/* ✅ Google Analytics 4 */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-8GJ4EQT6X3"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8GJ4EQT6X3', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
         {/* Hoặc nếu bạn muốn dùng PNG (rõ hơn) */}
         {/* <link rel="icon" href="/favicon.png" sizes="512x512" type="image/png" /> */}
   </head>
